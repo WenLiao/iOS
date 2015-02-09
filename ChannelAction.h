2370f68907a83832efa27c1e344d109dcb091f97
jimliao <jimliao@jimliaotekiMacBook-Pro-2.local>
2015-02-09 12:44:41 -0800

    Upload 2D customize code

---
+++ ChannelAction.h
@@ -0,0 +1,19 @@
+//
+//  ChannelAction.h
+//  Mod
+//
+//  Created by abner on 12/9/19.
+//  Copyright (c) 2012年 abner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChannelAction : NSObject<UIScrollViewDelegate>{
+    NSMutableArray *queue;
+    float lastContentOffset;
+}
+
+@property (assign, nonatomic) float timeLineOffset;
+@property (assign, nonatomic) UIScrollView *timeLineScale;
+@property (assign, nonatomic) UITableView *gridView;
+@end
